353f41f1a87ed864d875ec78b75274a953bb299e
LordKuyQ <112600699+LordKuyQ@users.noreply.github.com>
2025-12-07 18:15:56 +0700

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -3,12 +3,6 @@ import TheWelcome from './components/TheWelcome.vue'
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-   
-  </header>
-
   <main>
     <TheWelcome />
   </main>
